a056d3fb03e88a9ef092c01442489195a82605e6
Daniel Lucas <46814233+ITBeauftragter@users.noreply.github.com>
2021-09-09 15:39:13 +0200

    add bundle.js

--- src/BootstrapAsset.php
+++ src/BootstrapAsset.php
@@ -20,61 +20,7 @@ class BootstrapAsset extends \yii\web\AssetBundle
     ];
     
     public $js = [
-        'dist/js/bootstrap.min.js'
+        'dist/js/bootstrap.min.js',
+        'dist/js/bootstrap.bundle.js'
     ];
-    
-    /**
-     * @var string CDN version for CDN mode, eg. `'4.1.3'`
-     */
-    public $cdnVersion = '5.1.1';
-    
-    /**
-     * @var bool Enable or disable CDN mode
-     */
-    public $cdn = false;
-    
-    /**
-     * @var array Sprintf format or fixed URL of Bootstrap CDN URL
-     */
-    public $cdnCSS = ['https://stackpath.bootstrapcdn.com/bootstrap/%s/css/bootstrap.min.css'];
-    
-    /**
-     * @var array Sprintf format or fixed URL of Bootstrap CDN URL
-     */
-    public $cdnJS = ['https://stackpath.bootstrapcdn.com/bootstrap/%s/js/bootstrap.min.js'];
-    
-    /**
-     * @var boolean Enable to auto depends \yidas\yii\jquery\JqueryAsset
-     * @see https://github.com/yidas/yii2-jquery
-     */
-    public $jquery = false;
-    
-    /**
-     * Source handler
-     */
-    public function init()
-    {
-        // CDN mode
-        if ($this->cdn) {
-            // Unset sourcePath
-            $this->sourcePath = NULL;
-            // Rewrite CSS
-            $this->css = [];
-            foreach ($this->cdnCSS as $key => $url) {
-                $this->css[] = sprintf($url, $this->cdnVersion);
-            }
-            // Rewrite JS
-            $this->js = [];
-            foreach ($this->cdnJS as $key => $url) {
-                $this->js[] = sprintf($url, $this->cdnVersion);
-            }
-        }
-        
-        // jQuery Dependency
-        if ($this->jquery) {
-             $this->depends[] = 'yidas\yii\jquery\JqueryAsset';
-        }
-        
-        parent::init();
-    }
 }
